0bc48a47acf0a262b557484c6d9991624b66a928
surabhikumari143 <91407263+surabhikumari143@users.noreply.github.com>
2021-09-26 19:09:44 +0530

    Add files via upload

---
+++ add number.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,n,sum=0;
+	printf("Enter the number :");
+	scanf("%d",&n);
+	for(;n!=0;)
+	{
+		i=n%10;
+		sum=sum+i;
+		n=n/10;
+	}
+	printf("The sum of the digit is : %d",sum);
+}
